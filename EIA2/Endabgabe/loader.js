434a73eeedc05e00d796c5c59c11e7d9f910e091
MarvRiegelQQ <48470436+MarvRiegelQQ@users.noreply.github.com>
2023-02-12 16:54:50 +0100

    server side, doesn't work

--- EIA2/Endabgabe/loader.js
+++ EIA2/Endabgabe/loader.js
@@ -1,25 +1,26 @@
 const options = document.getElementsByName('options');
-const container = document.getElementById('container');
+const inputs = document.getElementById('container');
 let currentScript = null;
+//Loads script depending on selected radio button
 for (const option of options) {
     option.addEventListener('change', (event) => {
         let url;
         const target = event.target;
         if (target.value === 'option1') {
-            url = 'trailingFW/particles.js';
+            url = 'trailingFW/trail.js';
         }
         else if (target.value === 'option2') {
             url = 'tester2/notrail.js';
         }
         if (currentScript) {
-            container.removeChild(currentScript);
+            inputs.removeChild(currentScript);
         }
         fetch(url)
             .then((response) => response.text())
             .then((script) => {
             const element = document.createElement('script');
             element.text = script;
-            container.appendChild(element);
+            inputs.appendChild(element);
             currentScript = element;
         })
             .catch((error) => {
--- EIA2/Endabgabe/loader.ts
+++ EIA2/Endabgabe/loader.ts
@@ -1,6 +1,8 @@
 const options = document.getElementsByName('options');
-const container = document.getElementById('container');
+const inputs = document.getElementById('container');
 let currentScript: HTMLScriptElement | null = null;
+
+//Loads script depending on selected radio button
 
 for (const option of options) {
   option.addEventListener('change', (event) => {
@@ -13,7 +15,7 @@ for (const option of options) {
     }
 
     if (currentScript) {
-      container.removeChild(currentScript);
+      inputs.removeChild(currentScript);
     }
 
     fetch(url)
@@ -21,11 +23,72 @@ for (const option of options) {
       .then((script) => {
         const element = document.createElement('script');
         element.text = script;
-        container.appendChild(element);
+        inputs.appendChild(element);
         currentScript = element;
       })
       .catch((error) => {
         console.error(error);
       });
   });
+
+  async function saveValues() {
+    const colorInput = document.getElementById("colorInput") as HTMLInputElement;
+    const colorValue = colorInput.value;
+  
+    const sizeInput = document.getElementById("sizeInput") as HTMLInputElement;
+    const sizeValue = sizeInput.value;
+  
+    const shapeInput = document.getElementById("shapeInput") as HTMLSelectElement;
+    const shapeValue = shapeInput.value;
+  
+    const options = document.querySelector('input[name="options"]:checked') as HTMLInputElement;
+    const optionValue = options.value;
+    
+    const values = { color: colorValue, size: sizeValue, shape: shapeValue, option: optionValue };
+    
+    const response = await fetch("https://your-server.com/save-values", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(values)
+    });
+    
+    if (response.ok) {
+      console.log("Values saved successfully!");
+    } else {
+      console.error("Failed to save values");
+    }
+  }
+  
+  async function loadValues() {
+    const response = await fetch("https://your-server.com/load-values");
+    const values = await response.json();
+    
+    const tableBody = document.getElementById("tableBody");
+    const row = document.createElement("tr");
+    
+    const colorCell = document.createElement("td");
+    colorCell.textContent = values.color;
+    
+    const sizeCell = document.createElement("td");
+    sizeCell.textContent = values.size;
+    
+    const shapeCell = document.createElement("td");
+    shapeCell.textContent = values.shape;
+    
+    const optionCell = document.createElement("td");
+    optionCell.textContent = values.option;
+    
+    row.appendChild(colorCell);
+    row.appendChild(sizeCell);
+    row.appendChild(shapeCell);
+    row.appendChild(optionCell);
+    
+    tableBody.appendChild(row);
+  }
+  
+  document.getElementById("saveButton").addEventListener("click", saveValues);
+  document.getElementById("loadButton").addEventListener("click", loadValues);
 }
+
